ead0ebafa310a4298a0636ae07d6e449b1dc4f68
Alan Chelko <achelko@gmail.com>
2023-09-16 12:45:10 +0000

    Final edits

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 // HINTs: Use the axios documentation as well as the video lesson to help you.
-// https://axios-http.com/docs/post_example
+// g
 // Use the Secrets API documentation to figure out what each route expects and how to work with it.
 // https://secrets-api.appbrewery.com/
 
